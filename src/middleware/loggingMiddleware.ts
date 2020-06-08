b094b14366eff6f8ef5c720d630601e51b36c137
jplasch <j.plasch@zoho.com>
2020-06-07 21:51:32 -0700

    reseting

--- src/middleware/loggingMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export function loggingMiddleware(req: Request, res: Response, next: NextFunction) {
-    console.log(`Request received to url: ${req.url} with method: ${req.method}`);
-    next();
-  }
